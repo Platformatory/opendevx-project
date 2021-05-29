4e9a42f67215860792236c50b35ffac703ea3e82
p6 <pavan@grahana.net>
2021-05-29 16:33:16 +0530

    permissions

--- post-build.sh
+++ post-build.sh
@@ -2,5 +2,5 @@ if [ ! -f ./web/sites/default/settings.php ]; then
     echo "copying settings.php"	
     cp ./web/sites/default/default.settings.php ./web/sites/default/settings.php
     chmod -R 755 ./web/sites/default
-    chmod 644 ./web/sites/default/settings.php
+    chmod 777 ./web/sites/default/settings.php
 fi
