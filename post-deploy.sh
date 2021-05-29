66e22617a10cb6110893fa0e1991a2d528740723
p6 <pavan@grahana.net>
2021-05-29 15:07:01 +0530

    fix fresh install process

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,10 @@
 cd web
 drush cget system.site
 if [ $? -ne 0 ]; then
-  drush si opendevx --site-name="ODX Demo" --account-pass=admin123 -y
+  cp ./sites/default/default.settings.php ./sites/default/settings.php
+  chmod -R 755 ./sites/default
+  chmod 644 ./sites/default/settings.php
+  drush si opendevx --site-name="OpenDevX Developer Exchange" --account-pass=admin123 -y
   drush cr
   drush cr
 else
